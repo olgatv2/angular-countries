a369dca6310d4c885a20e99a1d31f03d355e4b11
Olga <olga.tello@sngular.com>
2024-07-25 19:01:00 +0200

    WIP: Countries table.

---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'countries-table',
+  templateUrl: 'country-table.component.html'
+})
+
+export class CountryTableComponent {
+  @Input()
+  public countries: Country[] = [];
+
+}
