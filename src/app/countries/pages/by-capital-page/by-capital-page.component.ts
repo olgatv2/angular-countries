314f9ec8f1c5a8712f1488ba6a9d4fd0d85ce367
Olga <olgatv2@gmail.com>
2024-09-18 18:51:28 +0200

    Data persist.

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedModule } from "../../../shared/shared.module";
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
@@ -8,11 +8,16 @@ import { Country } from '../../interfaces/country.interface';
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
   public countries: Country[] = []
   public isLoading: boolean = false
+  public initialValue: string = ''
 
   constructor(private countriesService: CountriesService) { }
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCapital.countries
+    this.initialValue = this.countriesService.cacheStore.byCapital.term
+  }
 
   searchByCapital(term: string): void {
     this.isLoading = true
